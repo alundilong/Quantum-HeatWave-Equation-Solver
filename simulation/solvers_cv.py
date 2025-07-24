fix: use factor of sqrt(2)

simulation/solvers_cv.py
@@ -289,7 +289,7 @@ class Solver1DLocal(Solver1D):
         self.st = StateProcessor(self.kwargs['nx'], self.kwargs['nt'], shift=1)
         self.st.set_u(self.kwargs['u'], 0)
         self.st.set_v(self.kwargs['v'], 0)
-        self.st.forward_state(0, self.tf.t @ self.tf.sqrt_m, factor = 2.0)
+        self.st.forward_state(0, self.tf.t @ self.tf.sqrt_m, factor = np.sqrt(2.0))
         self.circuit_groups = []
         self.logger.info('Initial state transformed.')
 
@@ -315,12 +315,13 @@ class Solver1DLocal(Solver1D):
         sampler, _ = backend.get_sampler()
         self.logger.info('Backend initialized.')
 
-        initial_state = self.st.get_state(0, factor = 2.0)
+        initial_state = self.st.get_state(0, factor = np.sqrt(2.0))
         N = len(initial_state)
         Psi_0 = np.zeros(2 * N, dtype=complex)
         Psi_0[:N] = initial_state  # Upper half is physical state
         Psi_0[N:] = initial_state
 
+        self.logger.info(f'initial_state Norm: {np.linalg.norm(initial_state):.2e}')
         self.logger.info(f'Psi0 Norm: {np.linalg.norm(Psi_0):.2e}')
         self.logger.info(f'Htest Norm: {np.linalg.norm(self.tf.h_test):.2e}')
         self.logger.info(f'Htilde Norm: {np.linalg.norm(self.tf.h_tilde):.2e}')
@@ -352,9 +353,8 @@ class Solver1DLocal(Solver1D):
         states_raw = tomo.run_tomography(result_groups, observables, self.times[1:])
         self.logger.info('Tomography completed.')
 
-        self.st.states = np.real(parallel_transport(states_raw, Psi_0))[:,N:]
+        self.st.states = np.real(parallel_transport(states_raw, Psi_0))[:,:N]
         self.logger.info('State polarization corrected.')
-        self.st.norm = self.st.norm*np.sqrt(2.0)
         _ = [self.st.inverse_state(i, self.tf.inv_sqrt_m @ self.tf.inv_t)
          for i in range(1, len(self.times))]
         self.logger.info('States inverse-transformed.')